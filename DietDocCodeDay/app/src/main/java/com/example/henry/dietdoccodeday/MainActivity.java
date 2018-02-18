752f736d46bfc4b58923cffde12f2889587baa97
hg1722 <henrygu1722@gmail.com>
2018-02-17 23:58:21 -0800

    1.0
    1.0

---
+++ DietDocCodeDay/app/src/main/java/com/example/henry/dietdoccodeday/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.henry.dietdoccodeday;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    FragmentManager manager = getSupportFragmentManager();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_camera:
+                    CameraFragment suggestedFragment = new CameraFragment();
+                    Intent intent = new Intent(MainActivity.this, CameraFragment.class);
+                    startActivity(intent);
+                    //manager.beginTransaction().replace(R.id.content, suggestedFragment, suggestedFragment.getTag()).commit();
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ DietDocCodeDay/app/src/main/java/com/example/henry/dietdoccodeday/SearchFragment.java
@@ -0,0 +1,8 @@
+package com.example.henry.dietdoccodeday;
+
+/**
+ * Created by Henry on 2/17/2018.
+ */
+
+public class SearchFragment {
+}
